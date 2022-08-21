82232c2baa0877d63dd90e67de739b3a705731c8
Isaiah Magana <maganaisaiah4@gmail.com>
2022-08-21 00:36:13 -0400

    Add Lecture 26 Class constructors and Named Arguments to the bottom of the file

--- our_first_app/lib/main.dart
+++ our_first_app/lib/main.dart
@@ -39,3 +39,45 @@ Cool thing about flutter is we dont have to render anything, or
     positon it manually, instead we just decide what goes on our screen, and how 
     without ever using the mouse
 */
+
+/*
+# 26. Class Constructors and arguments
+"MaterialApp and Text are not functions but classes"
+- Yet you can pass data to them?
+Important: If you do get a "non nullable field must be intialized" error, adjust the code to this
+<type>? <name>;
+
+Constructors - function in a class (or method) which is diffrent to others bc. it always executes only when we create a new obj. based on a class
+Ex:
+Person(String inputName, int inputAge) { //You should make the parameters in the constructor diffrent from the properties of the class
+  name = inputName;
+  age = inputAge;
+}
+**If you do use the same name in the parameters of the constructor use this.
+  I.E this.name = name
+**An example for us is Text - it passes the string we give it to the Text Class constructor
+
+## Named Arguments:
+Person({String inputName, int inputAge}) { //You should make the parameters in the constructor diffrent from the properties of the class
+  name = inputName;
+  age = inputAge;
+}
+
+When you call you have to do
+var person1 = Person(inputName: 'Jacob', inputAge: 19)
+Why would you use them?
+- functions or constructors which take alot of arguments bc. in a normal situation you have to remember each argument and their positions
+    - While easy with 2, if you look at an app even one like ours you could have hundreds of arguments
+- Allow you to mix and match order unlike for positional arguments
+With named arguments since they are optional you should define default values for each in the argument list on the constructor
+Or you could do @required <type> <name>, so it screams at you to do it
+- Note: Not a dart feature, @required is a flutter item
+
+## Named Argument shortcut(came about bc. how popular this is in dart over reg. args.)
+class Person {
+  String? name;
+  int? age;
+
+  Person({this.name='John Doe', this.age = 18});
+}
+*/
